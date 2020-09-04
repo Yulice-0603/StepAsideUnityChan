d909c8960355230c085efb884c0ef889610853dc
Yulice-0603 <69956727+Yulice-0603@users.noreply.github.com>
2020-09-04 22:27:40 +0900

    Initial commit

---
+++ Assets/ItemDestroyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyController : MonoBehaviour
+{
+
+    GameObject maincamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.maincamera = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.z < this.maincamera.transform.position.z)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
